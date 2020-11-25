4a2cb56b96d42901a45f762e3a279208f486fad7
Konstantin S <HavenDV@users.noreply.github.com>
2020-11-25 15:10:37 +1000

    Initialized.

---
+++ src/libs/H.Notifiers.TcpPingNotifier/TcpPingNotifier.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace H.Notifiers
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TcpPingNotifier : TimerNotifier
+    {
+        #region Properties
+
+        private string Ip { get; set; } = string.Empty;
+        private int Port { get; set; }
+        private bool OnlyIfNetworkActive { get; set; } = true;
+
+        #endregion
+
+        #region Contructors
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public TcpPingNotifier()
+        {
+            AddSetting(nameof(Ip), o => Ip = o, _ => true, Ip);
+            AddSetting(nameof(Port), o => Port = o, _ => true, Port);
+            AddSetting(nameof(OnlyIfNetworkActive), o => OnlyIfNetworkActive = o, _ => true, OnlyIfNetworkActive);
+        }
+
+        #endregion
+
+        #region Protected methods
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        protected override bool OnResult()
+        {
+            var isAvailableNetworkActive = GetVariable("$internet$", true);
+            if (OnlyIfNetworkActive && !isAvailableNetworkActive)
+            {
+                return false;
+            }
+
+            try
+            {
+                using (new TcpClient(Ip, Port))
+                {
+                }
+
+                return false;
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch (Exception)
+#pragma warning restore CA1031 // Do not catch general exception types
+            {
+                return true;
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ src/libs/H.Notifiers.TimerNotifier/TimerNotifier.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Timers;
+using H.Core.Notifiers;
+
+namespace H.Notifiers
+{
+    public class TimerNotifier : Notifier
+    {
+        #region Properties
+
+        private int _intervalInMilliseconds;
+        public int IntervalInMilliseconds {
+            get => _intervalInMilliseconds;
+            set {
+                _intervalInMilliseconds = value;
+
+                if (value <= 0)
+                {
+                    return;
+                }
+
+                Timer?.Dispose();
+                Timer = new Timer(value);
+                Timer.Elapsed += OnElapsed;
+                Timer.Start();
+            }
+        }
+
+        public int Frequency { get; set; }
+        public int RequiredCount { get; set; }
+
+        private Timer? Timer { get; set; }
+
+        private int CurrentCount { get; set; }
+        private DateTime LastEventTime { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public TimerNotifier()
+        {
+            AddSetting(nameof(IntervalInMilliseconds), o => IntervalInMilliseconds = o, Positive, int.MaxValue);
+            AddSetting(nameof(Frequency), o => Frequency = o, NotNegative, 0);
+            AddSetting(nameof(RequiredCount), o => RequiredCount = o, Positive, 1);
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        public override void Dispose()
+        {
+            base.Dispose();
+
+            Timer?.Stop();
+            Timer?.Dispose();
+            Timer = null;
+
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+        #region Protected methods
+
+        protected virtual Task<bool> OnResultAsync()
+        {
+            return Task.FromResult(true);
+        }
+
+        protected virtual bool OnResult()
+        {
+            return true;
+        }
+
+        private async void OnElapsed(object sender, ElapsedEventArgs e)
+        {
+            try
+            {
+                var value = OnResult() && await OnResultAsync().ConfigureAwait(false);
+                if (!value)
+                {
+                    CurrentCount = 0;
+                    return;
+                }
+
+                CurrentCount++;
+                if (CurrentCount < RequiredCount ||
+                    DateTime.Now < LastEventTime.AddMilliseconds(Frequency))
+                {
+                    return;
+                }
+
+                OnEventOccurred();
+                LastEventTime = DateTime.Now;
+                CurrentCount = 0;
+            }
+#pragma warning disable CA1031 // Do not catch general exception types
+            catch (Exception exception)
+#pragma warning restore CA1031 // Do not catch general exception types
+            {
+                Log($"Exception: {exception}");
+
+                OnExceptionOccurred(exception);
+
+                Log($"Disabling module: {Name}");
+                Disable();
+
+                CurrentCount = 0;
+            }
+        }
+
+        #endregion
+
+    }
+}
